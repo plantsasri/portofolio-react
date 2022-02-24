ae0627f5ca5993e2a100e7a8715989c18734c3f6
/notescepot <99325101+plantsasri@users.noreply.github.com>
2022-02-25 05:54:31 +0700

    test upload

---
+++ src/pages/BlogDetails.js
@@ -0,0 +1,57 @@
+import Disqus from "disqus-react";
+import Markdown from "markdown-to-jsx";
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import { useParams } from "react-router-dom";
+import Layout from "../components/Layout";
+
+function BlogDetails(props) {
+  const params = useParams();
+  const [content, setContent] = useState("");
+  const blogId = params.id;
+  const blogFile = params.title;
+  const fileName = `${blogFile}.md`;
+
+  useEffect(() => {
+    import(`../blog/${fileName}`)
+      .then((res) => {
+        fetch(res.default)
+          .then((res) => res.text())
+          .then((res) => setContent(res))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  });
+
+  const disqusShortname = "chester-react"; //found in your Disqus.com dashboard
+  const disqusConfig = {
+    url: "https://tf-react-chester.now.sh/", //Homepage link of this site.
+    identifier: blogId,
+    title: blogFile,
+  };
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Blog Details - AS React Portfolio</title>
+        <meta
+          name="description"
+          content="AS React Personal Portfolio"
+        />
+      </Helmet>
+      <div className="mi-blog-details mi-section mi-padding-top mi-padding-bottom">
+        <div className="container">
+          <Markdown>{content}</Markdown>
+          <div className="mi-blog-details-comments mt-30">
+            <Disqus.DiscussionEmbed
+              shortname={disqusShortname}
+              config={disqusConfig}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default BlogDetails;
---
+++ src/pages/Blogs.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import BlogsView from "../components/BlogsView";
+import Layout from "../components/Layout";
+import Pagination from "../components/Pagination";
+import Sectiontitle from "../components/Sectiontitle";
+
+function Blogs() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
+
+  useEffect(() => {
+    let mounted = true;
+    axios.get("/api/blog").then((response) => {
+      if (mounted) {
+        setPosts(response.data);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (e, pageNumber) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Blogs - AS React Portfolio</title>
+        <meta
+          name="description"
+          content="AS React Personal Portfolio"
+        />
+      </Helmet>
+      <div className="mi-about mi-section mi-padding-top mi-padding-bottom">
+        <div className="container">
+          <Sectiontitle title="Recent Blogs" />
+          <BlogsView blogs={currentPosts} />
+          {!(posts.length > postsPerPage) ? null : (
+            <Pagination
+              className="mt-50"
+              itemsPerPage={postsPerPage}
+              totalItems={posts.length}
+              paginate={paginate}
+              currentPage={currentPage}
+            />
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Blogs;
